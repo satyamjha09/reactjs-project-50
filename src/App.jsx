c87310f2f6749f8947be9a53daaef3e23075140b
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-07-16 17:57:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import Hert from './Hert';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Instagram-like Heart Animation</h1>
+      <Hert />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Hert.jsx
@@ -0,0 +1,39 @@
+// src/Heart.jsx
+import React, { useState } from 'react';
+import './Hert.css';
+
+const Heart = () => {
+
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+    }, 800);
+    
+  };
+
+  return (
+    
+    <div className={`heart-container ${clicked ? 'active' : '' } `} onClick={handleClick}>
+
+      <div className="heart">
+        <svg viewBox="0 0 24 24" className={`heart-svg ${clicked ? 'active' : ''}`}>
+          <path
+            fill="none"
+            stroke="#FF385C"
+            strokeWidth="2"
+            d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+          />
+        </svg>
+      </div>
+
+      {clicked && <div className="double-click-text">Liked!</div>}
+
+    </div>
+  );
+};
+
+export default Heart;
